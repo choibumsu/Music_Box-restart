77e7525ae307738f403432101eee5c336244195e
bumsu <bumsu0211@gmail.com>
2019-07-30 10:35:09 +0900

    update templates

--- musicbox/urls.py
+++ musicbox/urls.py
@@ -5,5 +5,6 @@ app_name = "musicbox"
 
 urlpatterns = [
     path('', views.musicbox_home, name="musicbox_home"),
-    path('song/', views.song_list, name="song_list"),
+    path('<str:kind>', views.kind_list, name="kind_list"),
+    path('<str:kind>/<int:pk>', views.kind_detail, name="kind_detail"),
 ]
--- musicbox/views.py
+++ musicbox/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Song, Artist, Genre, Playlist
 
 # Create your views here.
@@ -11,9 +12,71 @@ def musicbox_home(request):
     }
     return render(request, "musicbox/musicbox_home.html", context=context)
 
-def song_list(request):
-    songs = Song.objexts.all()
-    return render(request, "musicbox/song_list.html", {
-        "songs" : songs,
-    })
+def kind_list(request, kind):
+    if kind=='song':
+        contexts = Song.objects.all()
+        PAGE_ROW_COUNT = 6
+    elif kind=='artist':
+        contexts = Artist.objects.all()
+        PAGE_ROW_COUNT = 10
+    elif kind=='genre':
+        contexts = Genre.objects.all()
+        PAGE_ROW_COUNT = 10
+    elif kind=='playlist':
+        contexts = Playlist.objects.all()
+        PAGE_ROW_COUNT = 10
+    else:
+        return redirect("musicbox:musicbox_home")
+    
+    PAGE_DISPLAY_COUNT = 10
+    
+    paginator=Paginator(contexts, PAGE_ROW_COUNT)
+    pageNum=request.GET.get('pageNum') # 현제 페이지
+    
+    totalPageCount=paginator.num_pages # 전체 페이지 갯수 
+    
+    try:
+        contexts = paginator.page(pageNum)
+    except PageNotAnInteger:
+        contexts = paginator.page(1)
+        pageNum = 1
+    except EmptyPage:
+        contexts = paginator.page(paginator.num_pages)
+        pageNum = paginator.num_pages
+        
+    pageNum = int(pageNum)
+    
+    startPageNum = 1 + PAGE_DISPLAY_COUNT * int((pageNum - 1)/PAGE_DISPLAY_COUNT)
+    endPageNum = startPageNum + PAGE_DISPLAY_COUNT-1
+    if totalPageCount < endPageNum:
+        endPageNum = totalPageCount
+        
+    bottomPages = range(startPageNum, endPageNum+1)
+    
+    return render(request, f"musicbox/{kind}_list.html", {
+            f'{kind}s':contexts,
+            'kind':kind,
+            'pageNum':pageNum,
+            'bottomPages':bottomPages,
+            'totalPageCount':totalPageCount,
+            'startPageNum':startPageNum,
+            'endPageNum':endPageNum
+        }
+    )
 
+def kind_detail(request, kind, pk):
+    if kind=='song':
+        context = get_object_or_404(Song, pk=pk)
+    elif kind=='artist':
+        context = get_object_or_404(Artist, pk=pk)
+    elif kind=='genre':
+        context = get_object_or_404(Genre, pk=pk)
+    elif kind=='playlist':
+        context = get_object_or_404(Playlist, pk=pk)
+    else:
+        return redirect("musicbox:musicbox_home")
+
+    return render(request, f"musicbox/{kind}_detail.html", {
+            f'{kind}':context,
+        }
+    )
